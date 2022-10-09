91d3791e9423de38484ea5ac9285280d732fd46e
Tamer AYAS <tamerayas01@gmail.com>
2022-10-09 19:20:28 +0300

    Edited routes and added redirect for product list

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import ProductList from '@/views/ProductList.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView,
+    name: 'Home',
+    redirect: () => {
+      return { path: '/product-list' };
+    }
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
-  },
+    path: '/product-list',
+    name: 'ProductList',
+    component: ProductList
+  }
 ];
 
 const router = createRouter({
